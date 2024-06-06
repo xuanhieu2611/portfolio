b4f67e571ee78a3c9606e3bcab8ca8f0cfe7cc47
xuanhieu2611 <hieulexuan261103@gmail.com>
2024-06-06 15:28:05 +0700

    add split the bills project

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -5,21 +5,29 @@ import {motion, useScroll, useSpring, useTransform} from "framer-motion"
 const items = [
     {
         id:1,
-        title:"VitAlert ",
+        title:"Split The Bills",
+        img:"/splitthebills.png",
+        description:"Split The Bills is a full-stack web application that make shared bills between students, roomates, and colleagues become much easier!",
+        githubLink: "https://github.com/xuanhieu2611/Split-The-Bills",
+        link: "https://split-the-bills-hieu.vercel.app/"
+    },
+    {
+        id:2,
+        title:"VitAlert",
         img:"/VitAlert.png",
         description:"VitAlert is a web application designed to help individuals maintain a balanced diet by tracking their daily food intake. VitAlert will display the total amount of specific nutrients, percentage of recommended daily intake achieved, and symptoms of deficiency of a specific nutrient. This additional information includes the deficiency rarity, deficiency symptoms , foods that contain the specific nutrient, and functions of the specific nutrient within the human body.",
         githubLink: "https://github.com/xuanhieu2611/VitAlert",
         link: "https://vitalert.tech/"
     },
     {
-        id:2,
+        id:3,
         title:"UBC Insight Website",
         img:"/ubc.png",
         description:"This is my CPSC 311 project - a full stack web allows users effectively query historical data regarding the university’s sections and rooms. I was mainly responsible for the back-end including creating RESTful API, load, validate, and parse data to process",
         githubLink: "https://github.com/xuanhieu2611/UBC-grade-insight",
     },
     {
-        id:3,
+        id:4,
         title:"Portfolio Website",
         img:"/portfolio.png",
         description:"A website that displays my personal information, contacts, and my projects",

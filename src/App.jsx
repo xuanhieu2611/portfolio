5609e03fe8c753580793439dda41ad7128a00433
xuanhieu2611 <hieulexuan261103@gmail.com>
2024-07-10 17:19:57 +0700

    add hero background, add framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,21 @@ import Hero from "./components/Hero"
 
 function App() {
   return (
-    <div className="bg-gray-50 text-gray-950">
-      <section id="Homepage">
+    <div className="text-gray-950">
+      <div className="absolute -z-10 h-full w-full bg-gray-50">
+        <div class="absolute inset-0 -z-10 bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_300px_at_70%_400px,#dcfce7,transparent)] sm:bg-[radial-gradient(circle_500px_at_70%_300px,#dcfce7,transparent)] lg:bg-[radial-gradient(circle_600px_at_70%_200px,#dcfce7,transparent)]"></div>
+        <div class="absolute inset-0 -z-10 bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_300px_at_30%_400px,#cffafe,transparent)] sm:bg-[radial-gradient(circle_500px_at_30%_300px,#cffafe,transparent)] lg:bg-[radial-gradient(circle_600px_at_30%_200px,#cffafe,transparent)]"></div>
+      </div>
+      <section id="Homepage" className="bg-transparent">
         <Header />
         <Hero />
       </section>
+      <div className="bg-gray-50">
       <About/>
       <Projects/>
       <Contact/>
       <Footer/>
+      </div>
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,13 @@
-import React from 'react'
+import { motion } from "framer-motion"
+
+const container = (delay) => ({
+  hidden: {x: -100, opacity: 0},
+  visible: {
+    x:0,
+    opacity: 1,
+    transition: {duration: 0.5, delay: delay}
+  },
+})
 
 export default function Hero() {
   return (
@@ -7,30 +16,64 @@ export default function Hero() {
         {/* Hero text */}
         <div className='flex flex-col items-center justify-center gap-8'>
           {/* Text */}
-          <h2 className='font-normal tracking-widest text-3xl pt-10 lg:text-5xl'>Hieu Le</h2>
-          <h1 className='font-bold text-4xl lg:text-6xl bg-gradient-to-br from-emerald-300 to-green-500 bg-clip-text text-transparent'>Software Engineer</h1>
-          <div className="flex gap-5">
+          <motion.h2 
+            variants={container(0)}
+            initial="hidden"
+            animate="visible"
+            className='font-normal tracking-widest text-3xl pt-10 lg:text-5xl'
+          >
+            Hieu Le
+          </motion.h2>
+          <motion.h1 
+            variants={container(0.2)}
+            initial="hidden"
+            animate="visible"
+            className='font-bold text-4xl lg:text-6xl bg-gradient-to-br from-emerald-300 to-green-500 bg-clip-text text-transparent'
+          >
+            Software Engineer
+          </motion.h1>
+          <motion.div 
+            variants={container(0.4)}
+            initial="hidden"
+            animate="visible"
+            className="flex gap-5"
+          >
             <button className='border-solid border-2 border-gray-700 p-2 rounded-xl hover:bg-green-500 hover:text-white hover:border-green-500' onClick={() => location.href='/#Projects'}>See the Latest Works</button>
             <button className='border-solid border-2 border-green-500 p-2 rounded-xl bg-green-500 text-white hover:bg-gray-50 hover:text-gray-950 hover:border-gray-700' onClick={() => location.href='/#Contact'}>Contact me</button>
-          </div>
+          </motion.div>
 
           {/* Social Media */}
-          <div className="flex gap-5">
+          <motion.div 
+            variants={container(0.6)}
+            initial="hidden"
+            animate="visible"
+            className="flex gap-5"
+          >
             <img src="/instagram.svg" alt="My Instagram" className='h-10 w-10 hover:cursor-pointer hover:scale-110 hover:-translate-y-1 transition ease-in-out duration-100' onClick={() => (window.open("https://instagram.com/hieu.spring"))}/>
             <img src="/linkedin.svg.png" alt="My Linkedin" className='h-10 w-10 hover:cursor-pointer hover:scale-110 hover:-translate-y-1 transition ease-in-out duration-100' onClick={() => (window.open("https://linkedin.com/in/hieule2611"))}/>
             <img src="/github.svg.png" alt="My Github" className='h-10 w-10 hover:cursor-pointer hover:scale-110 hover:-translate-y-1 transition ease-in-out duration-100' onClick={() => (window.open("https://github.com/xuanhieu2611"))}/>
-          </div>
+          </motion.div>
         </div>
 
         {/* Hero image */}
         <div className="flex w-48 overflow-hidden items-center justify-center">
-          <img src="/ava.jpg" alt="My Portrait" className='object-fit rounded-full'/>
+          <motion.img 
+            initial={{x:100, opacity:0}}
+            animate={{x:0, opacity:1}}
+            transition={{duration:0.5, delay:0.5}}
+            src="/ava.jpg" alt="My Portrait" className='object-fit rounded-full'
+          />
         </div>
 
       </div>
 
       {/* Tech Stack */}
-      <div className=' hidden sm:flex sm:gap-5 lg:gap-10 items-center justify-center pt-32'>
+      <motion.div 
+        initial={{y:100, opacity:0}}
+        animate={{y:0, opacity:1}}
+        transition={{duration:0.5, delay:0.5}}
+        className=' hidden sm:flex sm:gap-5 lg:gap-10 items-center justify-center pt-32'
+      >
         <h2 className='text-2xl text-gray-600'>Tech Stack |</h2>
         <img src="./javascript.png" alt="javacript" className="h-10" />
         <img src="./typescript.jpg" alt="typescript" className="h-10" />
@@ -40,7 +83,7 @@ export default function Hero() {
         <img src="./python.png" alt="python" className="h-10" />
         <img src="./cpp.png" alt="c plus plus" className="h-10" />
         <img src="./mysql.png" alt="mysql" className="h-10" />
-      </div>
+      </motion.div>
     </>
   )
 }

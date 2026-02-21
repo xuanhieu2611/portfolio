9c3b74a992a75eaeed8471e89cca7fbc730546dd
xuanhieu2611 <hieulexuan261103@gmail.com>
2026-02-20 23:30:58 -0800

    adding ubc exp and strideai

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -1,10 +1,25 @@
 export const experiences = [
   {
+    company: "Stride AI",
+    role: "Founder",
+    period: "Present",
+    description: "Building AI-powered running coach",
+    technologies: ["Swift", "Firebase"],
+  },
+  {
+    company: "University of British Columbia",
+    role: "Software Developer Co-op",
+    period: "Jan 2026 - April 2026",
+    description:
+      "Building Fac360 - platform that gather and auto generate CVs and reports for professors and students accross Faculty of Medicine",
+    technologies: ["Next.js", "AWS", "Python"],
+  },
+  {
     company: "Teck Resources Ltd.",
     role: "Software Developer Co-op",
     period: "Dec 2024 - Aug 2025",
     description:
-      "Developed automation tools, data pipelines, and analytics solutions using Python, Power Automate, and Power BI to improve recruiting operations—cutting process times by up to 40%, reducing data discrepancies by 85%, and enabling data-driven decisions through interactive dashboards. Experienced in Agile practices, system integrations, and delivering scalable software solutions that optimize workflows.",
+      "Developed an event-driven sync tool between two ATSs systems using Python and Power Automate and built Power BI dashboards to visualize the performance and ROI of recruiting software tools",
     technologies: ["Python", "Power Automate", "Power BI", "Azure DevOps"],
   },
 ]
